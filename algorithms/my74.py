cf6e616a097dc9a12c9c4e2df65101bc9d12d37a
Will <changwei.cn@gmail.com>
2016-12-13 09:04:34 +0800

    第7章算法

--- algorithms/my74.py
+++ algorithms/my74.py
@@ -51,10 +51,10 @@ data = numpy.array([[1, 2],
 for i in range(m):
     merge(data[i][0], data[i][1])
 
-sum=0
+sum = 0
 
-for i in range(1, n+1):
-    if f[i]==i:
-        sum +=1
+for i in range(1, n + 1):
+    if f[i] == i:
+        sum += 1
 
-print (sum)
+print(sum)
